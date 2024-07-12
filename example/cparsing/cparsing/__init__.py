f8d8cdff9e15c4d49b49ffb44f6267f0a1e44742
Sachaa-Thanasius <111999343+Sachaa-Thanasius@users.noreply.github.com>
2024-07-12 16:07:22 -0400

    Remove cparsing.

--- example/cparsing/cparsing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""An attempt at making a C parser with sly. Heavily based on pycparser, which uses ply."""
-
-from .c_context import CContext, parse, parse_file, preprocess_file
-
-__all__ = ("CContext", "parse", "preprocess_file", "parse_file")
